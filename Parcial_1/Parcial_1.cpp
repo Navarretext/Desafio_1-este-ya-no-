c36c447557cebc15ab8331710b8f67cf745a344f
Navarretext <162393299+Navarretext@users.noreply.github.com>
2024-03-25 15:36:09 -0500

    Creacion archivos
    En Parcial_1.h se declaran las funciones, en Parcial_1.cpp se definen dichas funciones, y en main.cpp se implementan las funciones.

---
+++ Parcial_1/Parcial_1.cpp
@@ -0,0 +1,11 @@
+//IMPLEMENTACION DE FUNCIONES
+#ifndef PRACTICA_2_CPP
+#define PRACTICA_2_CPP
+#include "Practica_2.h"
+
+
+
+
+
+
+#endif // PRACTICA_2_CPP
---
+++ Parcial_1/Parcial_1.h
@@ -0,0 +1,19 @@
+#ifndef PARCIAL_1_H
+#define PARCIAL_1_H
+
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // PARCIAL_1_H
